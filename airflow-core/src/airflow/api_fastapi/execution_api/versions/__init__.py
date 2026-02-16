22c9aafaa5fc732b19452e6581a29dbdca33590e
Anish <giri.girianish@gmail.com>
2026-02-15 22:32:41 -0600

    Support map_index_template for mapped task groups (#40799)

--- airflow-core/src/airflow/api_fastapi/execution_api/versions/__init__.py
+++ airflow-core/src/airflow/api_fastapi/execution_api/versions/__init__.py
@@ -39,9 +39,11 @@ from airflow.api_fastapi.execution_api.versions.v2026_03_31 import (
     ModifyDeferredTaskKwargsToJsonValue,
     RemoveUpstreamMapIndexesField,
 )
+from airflow.api_fastapi.execution_api.versions.v2026_04_15 import AddTaskGroupMapIndexTemplateFields
 
 bundle = VersionBundle(
     HeadVersion(),
+    Version("2026-04-15", AddTaskGroupMapIndexTemplateFields),
     Version(
         "2026-03-31",
         MakeDagRunStartDateNullable,
